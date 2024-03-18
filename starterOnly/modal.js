8e850131d02113946e7d44fdee36da649f34499c
Cha-93 <153185886+Cha-93@users.noreply.github.com>
2024-03-18 16:13:17 +0100

    modal Close

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,34 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalBtnClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// ligne suivante action fermeture pop up click X
+modalBtnClose.addEventListener("click", closeModal);
+
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    // .bground{
+    //  display: "block" ;
+   // }
+    modalbg.style.display = "block";
 }
 
+function closeModal() {
 
+    modalbg.style.display = "none";
+
+}
